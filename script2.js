00edca696ca1efe24b5807b092685a106c6708fa
Deeneth <dineshkumars3870@gmail.com>
2025-12-03 21:33:12 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,25 @@
+const { validateInputTools } = require("openai/lib/ResponsesParser.js");
+
+let a=10;
+let b=15;
+let sum=a+b;
+console.log("the sum is: "+sum);
+
+let c=30;
+let d=25;
+console.log(c+d);
+
+function greet()
+{
+    console.log("Good morning!")
+}
+greet();
+
+function add(a,b)
+{
+    return a+b;d
+}
+
+const s=add(10,20);
+const greeting=() => { console.log("Hello!");};
+
