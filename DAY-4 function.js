0a8dbc46e78a39df05fdee8b616c321fe13aba7c
Deeneth <dineshkumars3870@gmail.com>
2025-12-05 09:16:08 +0530

    Add files via upload

---
+++ DAY-4 function.js
@@ -0,0 +1,15 @@
+function greet()
+{
+    console.log("hello!");
+}
+greet();
+
+let greet2 = function() {
+    console.log("hi there");
+};
+greet2();
+
+let greet3=()=>{
+    console.log("Hello from greet3");
+}
+greet3();
